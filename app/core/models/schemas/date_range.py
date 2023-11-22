a0aa752d46ad649dee2f11d809f62e516c5429ac
sidelkin <sidelkin@yandex.ru>
2023-11-22 13:36:51 +0400

    Added validation of date ordering

--- app/core/models/schemas/date_range.py
+++ app/core/models/schemas/date_range.py
@@ -1,7 +1,8 @@
 from datetime import date
+from typing import Self
 
 from dateutil.relativedelta import relativedelta
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, model_validator
 
 FROM_TIME = date.today().replace(day=1) - relativedelta(months=12)
 TO_TIME = date.today().replace(day=1) - relativedelta(months=1)
@@ -12,3 +13,11 @@ class DateRange(BaseModel):
     date_to: date = Field(..., examples=[TO_TIME])
 
     model_config = ConfigDict(extra="forbid")
+
+    @model_validator(mode="after")
+    def check_date_ordering(self) -> Self:
+        if self.date_from > self.date_to:
+            raise ValueError(
+                "`date_from` must be less than or equal to `date_to`"
+            )
+        return self

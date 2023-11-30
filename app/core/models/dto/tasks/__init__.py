c699f4bc079e3b5a6fce57711938714d4d583eae
sidelkin <sidelkin@yandex.ru>
2023-11-30 16:58:08 +0400

    Removed unnecessary task holder

--- app/core/models/dto/tasks/__init__.py
+++
@@ -1,9 +0,0 @@
-from .database import TaskDatabase
-from .excel import TaskExcel
-from .holder import TaskHolderDTO
-from .report import TaskReport
-
-task_holder = TaskHolderDTO()
-task_holder.add(TaskDatabase)
-task_holder.add(TaskExcel)
-task_holder.add(TaskReport)

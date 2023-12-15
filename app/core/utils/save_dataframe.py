8ef6df1933a6220989144ce3041fb929360f676c
sidelkin <sidelkin@yandex.ru>
2023-12-15 11:08:49 +0400

    Removed multi-line wrapping

--- app/core/utils/save_dataframe.py
+++ app/core/utils/save_dataframe.py
@@ -9,10 +9,7 @@ from app.core.config.settings import settings
 
 async def save_to_csv(df: pd.DataFrame, path: Path) -> None:
     async with aiofiles.open(
-        path,
-        mode="w",
-        encoding=settings.csv_encoding,
-        newline="",
+        path, mode="w", encoding=settings.csv_encoding, newline=""
     ) as afp:
         writer = AsyncWriter(afp, delimiter=settings.csv_delimiter)
         await writer.writerow(df.columns.to_list())

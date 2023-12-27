eb213ba841c811e1ff88eeab786d7126172aeacf
sidelkin <sidelkin@yandex.ru>
2023-12-27 18:24:34 +0400

    Removed license info from reservoir query

--- app/infrastructure/db/dao/query/querysets/common.py
+++ app/infrastructure/db/dao/query/querysets/common.py
@@ -14,9 +14,7 @@ MAX_NUMBER_OF_ORA = 3
 
 
 def select_description(
-    model: type[Base],
-    field_id: str,
-    field_descr: str = "description",
+    model: type[Base], field_id: str, field_descr: str = "description"
 ) -> ScalarSelect:
     return (
         select(getattr(DictG, field_descr))
@@ -26,10 +24,20 @@ def select_description(
     )
 
 
-def select_well_name(
-    model: type[Base],
-    field_id: str,
-) -> ScalarSelect:
+def select_reservoir(model: type[Base], field_id: str) -> ScalarSelect:
+    return (
+        select(
+            func.udmurtneft_n.dg_sdes(
+                func.decode(DictG.mr, None, DictG.id, DictG.mr)
+            )
+        )
+        .where(getattr(model, field_id) == DictG.id)
+        .scalar_subquery()
+        .correlate(model)
+    )
+
+
+def select_well_name(model: type[Base], field_id: str) -> ScalarSelect:
     return (
         select(WellHdr.well_name)
         .where(getattr(model, field_id) == WellHdr.uwi)
@@ -39,10 +47,10 @@ def select_well_name(
 
 
 def select_cids() -> ScalarSelect:
-    subqs = [
-        select_description(WellStockHistExt, f"ora{num}", field_descr="sdes")
+    subqs = (
+        select_reservoir(WellStockHistExt, f"ora{num}")
         for num in range(1, MAX_NUMBER_OF_ORA + 1)
-    ]
+    )
     # sourcery skip: use-fstring-for-concatenation
     return reduce(
         lambda result, subq: func.decode(

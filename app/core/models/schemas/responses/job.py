0af0913490602456a61da8b99d6013a21aef964f
sidelkin1 <91370354+sidelkin1@users.noreply.github.com>
2024-01-06 14:44:00 +0400

    Removed alias

--- app/core/models/schemas/responses/job.py
+++ app/core/models/schemas/responses/job.py
@@ -1,7 +1,6 @@
 from typing import Any, Self
 
 from arq.jobs import Job
-from pydantic import ConfigDict, Field
 
 from app.core.models.dto import JobStamp
 from app.core.models.enums import JobStatus
@@ -10,22 +9,18 @@ from app.core.models.schemas.responses.task import TaskResponse, TaskT
 
 
 class JobResponse(BaseResponse[dict[str, Any]]):
-    task: dict[str, Any] | None = Field(alias="data", default=None)
-
-    model_config = ConfigDict(populate_by_name=True)
-
     @classmethod
     async def from_job(cls, job: Job) -> Self:
         status = JobStatus.from_arq(await job.status())
         if status is JobStatus.not_found:
-            data = None
+            task = None
             job_stamp = JobStamp(
                 job_id=job.job_id, file_id=None, status=status
             )
         else:
             info = await job.info()
             response: TaskResponse[TaskT] = info.args[0]
-            data = response.task.model_dump()
+            task = response.task.model_dump()
             job_stamp = response.job
             if status is JobStatus.in_progress:
                 job_stamp.status = JobStatus.in_progress
@@ -34,4 +29,4 @@ class JobResponse(BaseResponse[dict[str, Any]]):
             else:
                 job_stamp.status = JobStatus.error
                 job_stamp.message = str(info.result)
-        return cls(data=data, job=job_stamp)
+        return cls(task=task, job=job_stamp)

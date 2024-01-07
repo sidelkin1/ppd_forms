15b06cdf589258c6aab111a0014dbf9602ceaf36
sidelkin1 <91370354+sidelkin1@users.noreply.github.com>
2024-01-07 14:15:41 +0400

    Changed way to get settings

--- app/core/config/settings.py
+++ app/core/config/settings.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 from pathlib import Path
 from typing import Any
 
@@ -74,4 +75,6 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
-settings = Settings()
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()

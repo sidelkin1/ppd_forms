02907670d54262da07f248223f8e7367a86b7094
sidelkin1 <91370354+sidelkin1@users.noreply.github.com>
2024-01-09 23:55:39 +0400

    Fixed building of DB URL

--- app/infrastructure/db/migrations/env.py
+++ app/infrastructure/db/migrations/env.py
@@ -1,15 +1,30 @@
 import asyncio
+import os
+import re
 from logging.config import fileConfig
 
 from alembic import context
+from dotenv import load_dotenv
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from app.infrastructure.db.models.local import Base
 
+load_dotenv()
+
 config = context.config
 
+url_tokens = {
+    "POSTGRES_DB": os.getenv("POSTGRES_DB", ""),
+    "POSTGRES_USER": os.getenv("POSTGRES_USER", ""),
+    "POSTGRES_PASSWORD": os.getenv("POSTGRES_PASSWORD", ""),
+    "DB_HOST": os.getenv("DB_HOST", ""),
+    "DB_PORT": os.getenv("DB_PORT", ""),
+}
+url = config.get_main_option("sqlalchemy.url")
+url = re.sub(r"\${(.+?)}", lambda m: url_tokens[m.group(1)], url)
+config.set_main_option("sqlalchemy.url", url)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)

ba2b9d03fc71ea10590b4da4f2dc4b74d6c2053f
sidelkin1 <91370354+sidelkin1@users.noreply.github.com>
2024-01-18 22:19:37 +0400

    Refactored tests for report services

--- tests/integration/test_reports.py
+++ tests/integration/test_reports.py
@@ -1,65 +1,51 @@
 from datetime import date
 from pathlib import Path
+from typing import Callable
 
 import pytest
 from csv_diff import compare, load_csv
-from sqlalchemy.orm import sessionmaker
 
 from app.core.config.settings import Settings
 from app.core.services.oil_loss_report import oil_loss_report
 from app.core.services.profile_report import profile_report
 from app.core.utils.process_pool import ProcessPoolManager
-from app.infrastructure.db.dao.sql.reporters import (
-    FirstRateLossReporter,
-    MaxRateLossReporter,
-    WellProfileReporter,
-)
-
-base_dir = Path(__file__).resolve().parent.parent.parent
-result_dir = base_dir / "tests" / "fixtures" / "resources" / "results"
-
-
-@pytest.mark.asyncio(scope="session")
-async def test_profile_report(
-    pool: sessionmaker,
-    process_pool: ProcessPoolManager,
-    tmp_path: Path,
-    settings: Settings,
-):
-    path = tmp_path / "results.csv"
-    dao = WellProfileReporter(pool)
-    date_from = date(2000, 1, 1)
-    date_to = date(2001, 1, 1)
-    await profile_report(path, date_from, date_to, dao, process_pool, settings)
-    diff = compare(
-        load_csv(open(result_dir / "profile_report.csv")), load_csv(open(path))
-    )
-    for value in diff.values():
-        assert not value
+from app.infrastructure.holder import HolderDAO
 
 
 @pytest.mark.parametrize(
-    "class_dao,expected_report",
+    "dao,service,expected_report",
     [
-        (FirstRateLossReporter, "first_rate_oil_loss.csv"),
-        (MaxRateLossReporter, "max_rate_oil_loss.csv"),
+        ("well_profile_reporter", profile_report, "profile_report.csv"),
+        (
+            "first_rate_loss_reporter",
+            oil_loss_report,
+            "first_rate_oil_loss.csv",
+        ),
+        ("max_rate_loss_reporter", oil_loss_report, "max_rate_oil_loss.csv"),
     ],
 )
 @pytest.mark.asyncio(scope="session")
-async def test_oil_loss_report(
-    pool: sessionmaker,
+async def test_reports(
+    pool_holder: HolderDAO,
     process_pool: ProcessPoolManager,
     tmp_path: Path,
     settings: Settings,
-    class_dao: type[FirstRateLossReporter] | type[MaxRateLossReporter],
+    dao: str,
+    service: Callable[..., None],
     expected_report: str,
 ):
+    base_dir = Path(__file__).resolve().parent.parent.parent
+    result_dir = base_dir / "tests" / "fixtures" / "resources" / "results"
     path = tmp_path / "results.csv"
-    dao = class_dao(pool)
     date_from = date(2000, 1, 1)
     date_to = date(2001, 1, 1)
-    await oil_loss_report(
-        path, date_from, date_to, dao, process_pool, settings
+    await service(
+        path,
+        date_from,
+        date_to,
+        getattr(pool_holder, dao),
+        process_pool,
+        settings,
     )
     diff = compare(
         load_csv(open(result_dir / expected_report)), load_csv(open(path))

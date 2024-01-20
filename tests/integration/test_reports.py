91bb3ffbf2084e96dc96ac586ecc0596e65b4417
sidelkin1 <91370354+sidelkin1@users.noreply.github.com>
2024-01-20 16:38:13 +0400

    Added tests for opp per year report

--- tests/integration/test_reports.py
+++ tests/integration/test_reports.py
@@ -7,21 +7,36 @@ from csv_diff import compare, load_csv
 
 from app.core.config.settings import Settings
 from app.core.services.oil_loss_report import oil_loss_report
+from app.core.services.opp_per_year_report import opp_per_year_report
 from app.core.services.profile_report import profile_report
 from app.core.utils.process_pool import ProcessPoolManager
+from app.infrastructure.db.dao.sql.reporters import LocalBaseDAO
 from app.infrastructure.holder import HolderDAO
 
 
 @pytest.mark.parametrize(
     "dao,service,expected_report",
     [
-        ("well_profile_reporter", profile_report, "profile_report.csv"),
+        (
+            "well_profile_reporter",
+            profile_report,
+            "profile_report.csv",
+        ),
         (
             "first_rate_loss_reporter",
             oil_loss_report,
             "first_rate_oil_loss.csv",
         ),
-        ("max_rate_loss_reporter", oil_loss_report, "max_rate_oil_loss.csv"),
+        (
+            "max_rate_loss_reporter",
+            oil_loss_report,
+            "max_rate_oil_loss.csv",
+        ),
+        (
+            "opp_per_year_reporter",
+            opp_per_year_report,
+            "opp_per_year_report.csv",
+        ),
     ],
 )
 @pytest.mark.asyncio(scope="session")
@@ -39,14 +54,8 @@ async def test_reports(
     path = tmp_path / "results.csv"
     date_from = date(2000, 1, 1)
     date_to = date(2001, 1, 1)
-    await service(
-        path,
-        date_from,
-        date_to,
-        getattr(pool_holder, dao),
-        process_pool,
-        settings,
-    )
+    dao_: LocalBaseDAO = getattr(pool_holder, dao)
+    await service(path, date_from, date_to, dao_, process_pool, settings)
     diff = compare(
         load_csv(open(result_dir / expected_report)), load_csv(open(path))
     )

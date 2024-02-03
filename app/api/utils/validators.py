cf61f9bd16d6b131e1081866672e34d92535f926
sidelkin1 <91370354+sidelkin1@users.noreply.github.com>
2024-02-03 16:09:23 +0400

    Cleaned

--- app/api/utils/validators.py
+++ app/api/utils/validators.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from fastapi import HTTPException, WebSocketException, status
+from fastapi import HTTPException, status
 
 from app.core.models.dto import UneftFieldDB
 
@@ -11,16 +11,6 @@ def check_file_exists(path: Path) -> None:
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Файл не найден!",
         )
-
-
-def check_user_id_exists_http(user_id: str) -> None:
-    if user_id is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-
-
-def check_user_id_exists_ws(user_id: str) -> None:
-    if user_id is None:
-        raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION)
 
 
 def check_field_exists(field_id: int, fields: list[UneftFieldDB]) -> None:

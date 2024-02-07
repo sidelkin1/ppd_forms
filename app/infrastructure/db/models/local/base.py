7c3735429006684eabe9ef40fa86b266358e67b5
sidelkin <sidelkin@yandex.ru>
2024-02-07 10:14:22 +0400

    Fixed migrations

--- app/infrastructure/db/models/local/base.py
+++ app/infrastructure/db/models/local/base.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import (
 from sqlalchemy.schema import Constraint
 
 convention: dict[str, str] = {
-    "ix": "ix__%(column_0_label)s",
+    "ix": "ix__%(table_name)s__%(column_0_N_name)s",
     "uq": "uq__%(table_name)s__%(column_0_name)s",
     "ck": "ck__%(table_name)s__%(constraint_name)s",
     "fk": "%(table_name)s_%(column_0_name)s_fkey",

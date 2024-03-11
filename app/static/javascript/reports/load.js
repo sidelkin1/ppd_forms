16c89dea9a16e9ec61412bb19ea1dfe3b1694e36
sidelkin <sidelkin@yandex.ru>
2024-03-11 15:24:44 +0400

    Added UI for matrix report

--- app/static/javascript/reports/load.js
+++ app/static/javascript/reports/load.js
@@ -56,3 +56,44 @@ async function loadOilLoss(reportName) {
   loader.classList.add("d-none");
   button.classList.remove("disabled");
 }
+
+async function loadMatrix(reportName) {
+  const loader = document.getElementById(`${reportName}Status`);
+  const button = document.getElementById(`${reportName}Button`);
+  const alert = document.getElementById(`${reportName}Danger`);
+  const success = document.getElementById(`${reportName}Success`);
+  const dateFrom = document.getElementById(`${reportName}Start`).value;
+  const dateTo = document.getElementById(`${reportName}End`).value;
+  const basePeriod = document.getElementById(`${reportName}Base`).value;
+  const predPeriod = document.getElementById(`${reportName}Pred`).value;
+  const onDate = document.getElementById(`${reportName}Mer`).value;
+  const link = document.getElementById(`${reportName}Link`);
+  const excludes = [
+    ...document.getElementById(`${reportName}Excludes`).selectedOptions,
+  ]
+    .filter((opt) => opt.value !== "--")
+    .map((opt) => opt.value);
+
+  loader.classList.remove("d-none");
+  button.classList.add("disabled");
+  alert.classList.add("d-none");
+  success.classList.add("d-none");
+
+  const url = `/reports/${reportName}`;
+  const data = {
+    date_from: dateFrom,
+    date_to: dateTo,
+    excludes: excludes,
+    base_period: basePeriod,
+    pred_period: predPeriod,
+    on_date: onDate,
+  };
+  const result = await assignWork(reportName, url, data);
+  if (result) {
+    link.href = `/reports/${result.job.file_id}`;
+    await checkStatus(reportName, result.job.job_id);
+  }
+
+  loader.classList.add("d-none");
+  button.classList.remove("disabled");
+}

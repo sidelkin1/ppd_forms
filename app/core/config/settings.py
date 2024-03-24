f74e0c9264d49ea90dd880ff43b6def844a90c5b
sidelkin1 <91370354+sidelkin1@users.noreply.github.com>
2024-03-24 17:30:10 +0400

    Added API host and port

--- app/core/config/settings.py
+++ app/core/config/settings.py
@@ -107,6 +107,9 @@ class Settings(BaseSettings):
     token_expire_time: timedelta = timedelta(days=7)
     secret_key: str
 
+    api_host: str = "127.0.0.1"
+    api_port: int = 8000
+
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 

ec1a34dd086e40654190e7e94dc0611b6d652b0e
sidelkin1 <91370354+sidelkin1@users.noreply.github.com>
2024-05-15 14:52:19 +0400

    Fixed imports

--- app/infrastructure/db/types/unify/__init__.py
+++ app/infrastructure/db/types/unify/__init__.py
@@ -1,5 +1,5 @@
-from .base_mapper import BaseMapper
+from .base_mapper import BaseMapper, SplitMode
 from .layer_mapper import LayerMapper
 from .regex_mapper import RegexMapper
 from .simple_mapper import SimpleMapper
-from .well_mapper import WellMapper
+from .well_mapper import TRANSLATE_TO, WellMapper

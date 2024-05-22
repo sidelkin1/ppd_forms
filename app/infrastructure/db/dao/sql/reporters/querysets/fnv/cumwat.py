e81ba90a51e28c97853a8521ccc42bdaeebb4e77
sidelkin <sidelkin@yandex.ru>
2024-05-22 16:45:23 +0400

    Fixed label for cumulative water

--- app/infrastructure/db/dao/sql/reporters/querysets/fnv/cumwat.py
+++ app/infrastructure/db/dao/sql/reporters/querysets/fnv/cumwat.py
@@ -5,7 +5,11 @@ from app.infrastructure.db.models.ofm.reflected import MonthlyInj
 
 def select_cumwat() -> Select:
     return (
-        select(MonthlyInj.field, MonthlyInj.uwi, func.sum(MonthlyInj.water))
+        select(
+            MonthlyInj.field,
+            MonthlyInj.uwi,
+            func.sum(MonthlyInj.water).label("cumwat"),
+        )
         .where(MonthlyInj.field == bindparam("field_id"))
         .group_by(MonthlyInj.field, MonthlyInj.uwi)
         .order_by(MonthlyInj.uwi)

1e1488f68977162f665c01472a630a0c304798d2
sidelkin <sidelkin@yandex.ru>
2024-06-19 09:28:45 +0400

    Added button to copy default folder

--- app/static/javascript/main.js
+++ app/static/javascript/main.js
@@ -1,3 +1,23 @@
 (() => {
   console.log("Sanity Check!");
 })();
+
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl, {
+    container: "body",
+    trigger: "hover",
+  });
+});
+
+function copyClipboard(id, text) {
+  navigator.clipboard.writeText(text);
+  let tooltip = bootstrap.Tooltip.getInstance(`#${id}`);
+  const oldTitle = tooltip._config.title;
+  tooltip._config.title = text;
+  tooltip.update();
+  tooltip.show();
+  tooltip._config.title = oldTitle;
+}

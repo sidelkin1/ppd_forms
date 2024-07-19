d9b8a01033c2c33f706531355f7b0c25555edd14
sidelkin <sidelkin@yandex.ru>
2024-07-19 17:07:09 +0400

    Renamed application setting fields

--- app/core/config/models/app.py
+++ app/core/config/models/app.py
@@ -10,8 +10,8 @@ class AppSettings(BaseSettings):
         default="Приложение для создания типовых отчетов ППД",
         validation_alias="app_description",
     )
-    delimiter: str = ","
-    max_workers: int = 4
-    root_path: str = ""
+    delimiter: str = Field(default=",", validation_alias="app_delimiter")
+    max_workers: int = Field(default=4, validation_alias="app_max_workers")
+    root_path: str = Field(default="", validation_alias="app_root_path")
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")

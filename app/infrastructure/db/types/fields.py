61686b8c5b29ee9679ffaf80b5a0f084bfa53916
sidelkin <sidelkin@yandex.ru>
2024-10-29 15:41:06 +0400

    ♻️ (db types): remove unnecessary global variable settings

--- app/infrastructure/db/types/fields.py
+++ app/infrastructure/db/types/fields.py
@@ -1,8 +1,5 @@
-from app.core.config.main import get_app_settings
 from app.infrastructure.db.mappers import field_mapper
 from app.infrastructure.db.types.base import BaseType
-
-settings = get_app_settings()  # FIXME avoid global variable
 
 
 class FieldType(BaseType):

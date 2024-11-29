9ef7b1dee6dc8805293bc4c775d78538c34b23ba
sidelkin <sidelkin@yandex.ru>
2024-11-29 17:19:34 +0400

    ♻️ fix(job): handle the case when `result_info is None` even if job status is not `not_found`

--- app/api/models/responses/job.py
+++ app/api/models/responses/job.py
@@ -1,6 +1,6 @@
-from typing import Any, Self, cast
+from typing import Any, Self
 
-from arq.jobs import Job, JobResult
+from arq.jobs import Job
 
 from app.api.models.responses.base import BaseResponse
 from app.core.models.dto import JobStamp, TaskBase
@@ -11,13 +11,13 @@ class JobResponse(BaseResponse[dict[str, Any]]):
     @classmethod
     async def from_job(cls, job: Job) -> Self:
         status = JobStatus.from_arq(await job.status())
-        if status is JobStatus.not_found:
+        info = await job.result_info()
+        if info is None:
             task = {}
             job_stamp = JobStamp(
                 job_id=job.job_id, status=status, created_at=None
             )
         else:
-            info = cast(JobResult, await job.info())
             response: BaseResponse[TaskBase] = info.args[0]
             task = response.task.model_dump()
             job_stamp = response.job

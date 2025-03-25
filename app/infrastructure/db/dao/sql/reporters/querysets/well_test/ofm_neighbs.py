3c0a2f727e7bca7a89af51e36f5c641b2f57e99d
sidelkin <sidelkin@yandex.ru>
2025-03-25 08:58:38 +0400

    ♻️ refactor(querysets): improve neighbour wells query logic

--- app/infrastructure/db/dao/sql/reporters/querysets/well_test/ofm_neighbs.py
+++ app/infrastructure/db/dao/sql/reporters/querysets/well_test/ofm_neighbs.py
@@ -5,7 +5,7 @@ from app.infrastructure.db.models.ofm.reflected import DictG, HeaderId, WellHdr
 
 
 def _select_well_coords() -> Subquery:
-    return (
+    subq = (
         select(
             HeaderId.field,
             HeaderId.cid,
@@ -17,27 +17,37 @@ def _select_well_coords() -> Subquery:
         .where(
             DictG.description == bindparam("field"),
             (
-                func.regexp_substr(WellHdr.well_name, r"^[^B]+")
+                func.regexp_replace(WellHdr.well_name, r"B\d+$", "")
                 == bindparam("well")
             ),
             HeaderId.cid.in_(bindparam("reservoirs")),
         )
     ).subquery()
+    return (
+        select(
+            subq.c.field,
+            subq.c.cid,
+            func.avg(subq.c.xcoord).label("xcoord"),
+            func.avg(subq.c.ycoord).label("ycoord"),
+        )
+        .group_by(subq.c.field, subq.c.cid)
+        .subquery()
+    )
 
 
-def select_ofm_neighbs() -> Select:
+def _select_well_neighbs() -> Subquery:
     subq = _select_well_coords()
-    radius = func.sqrt(
+    distance = func.sqrt(
         func.power(HeaderId.xcoord - subq.c.xcoord, 2)
         + func.power(HeaderId.ycoord - subq.c.ycoord, 2)
     )
-    well_no_branch = func.regexp_substr(WellHdr.well_name, r"^[^B]+")
+    well_no_branch = func.regexp_replace(WellHdr.well_name, r"B\d+$", "")
     return (
         select(
             DictG.description.label("field"),
             well_no_branch.label("well"),
             HeaderId.cid.label("reservoir"),
-            func.round(radius, 1).label("distance"),
+            func.round(distance, 1).label("distance"),
         )
         .join(WellHdr, WellHdr.uwi == HeaderId.uwi)
         .join(DictG, DictG.id == HeaderId.field)
@@ -49,8 +59,21 @@ def select_ofm_neighbs() -> Select:
             ),
         )
         .where(
-            radius <= bindparam("radius"),
+            distance <= bindparam("radius"),
             well_no_branch != bindparam("well"),
         )
-        .order_by(HeaderId.cid)
+    ).subquery()
+
+
+def select_ofm_neighbs() -> Select:
+    subq = _select_well_neighbs()
+    return select(
+        subq.c.field,
+        subq.c.well,
+        subq.c.reservoir,
+        func.avg(subq.c.distance).label("distance"),
+    ).group_by(
+        subq.c.field,
+        subq.c.well,
+        subq.c.reservoir,
     )

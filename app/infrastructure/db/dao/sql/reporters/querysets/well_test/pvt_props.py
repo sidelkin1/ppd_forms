b090559229563a0584432fbe2d8f35e3a1c575a4
sidelkin <sidelkin@yandex.ru>
2025-03-25 08:56:27 +0400

    🐛 fix(well_test): correct well name regex in PVT property selection

--- app/infrastructure/db/dao/sql/reporters/querysets/well_test/pvt_props.py
+++ app/infrastructure/db/dao/sql/reporters/querysets/well_test/pvt_props.py
@@ -13,20 +13,27 @@ from app.infrastructure.db.models.ofm.reflected import (
 
 def select_pvt_props() -> Select:
     dictg_alias = aliased(DictG)
-    return select(
-        HeaderId.cid.label("reservoir"),
-        ResPty.initial_layer_pressure.label("p_init"),
-        ResPty.bubble_point_pressure.label("p_bubble"),
-    ).where(
-        HeaderId.uwi == WellHdr.uwi,
-        DictG.id == HeaderId.field,
-        dictg_alias.sdes == HeaderId.cid,
-        DictG.description == bindparam("field"),
-        func.regexp_substr(WellHdr.well_name, r"^[^B]+") == bindparam("well"),
-        dictg_alias.sdes.in_(bindparam("reservoirs")),
-        ResPty.reservoir_s == Reservoir2.reservoir_s,
-        Reservoir2.field_code == HeaderId.field,
-        Reservoir2.district == HeaderId.district_id,
-        Reservoir2.existence_type == "ACTUAL",
-        Reservoir2.reservoir_id == dictg_alias.id,
+    return (
+        select(
+            HeaderId.cid.label("reservoir"),
+            ResPty.initial_layer_pressure.label("p_init"),
+            ResPty.bubble_point_pressure.label("p_bubble"),
+        )
+        .where(
+            HeaderId.uwi == WellHdr.uwi,
+            DictG.id == HeaderId.field,
+            dictg_alias.sdes == HeaderId.cid,
+            DictG.description == bindparam("field"),
+            (
+                func.regexp_replace(WellHdr.well_name, r"B\d+$", "")
+                == bindparam("well")
+            ),
+            dictg_alias.sdes.in_(bindparam("reservoirs")),
+            ResPty.reservoir_s == Reservoir2.reservoir_s,
+            Reservoir2.field_code == HeaderId.field,
+            Reservoir2.district == HeaderId.district_id,
+            Reservoir2.existence_type == "ACTUAL",
+            Reservoir2.reservoir_id == dictg_alias.id,
+        )
+        .distinct()
     )

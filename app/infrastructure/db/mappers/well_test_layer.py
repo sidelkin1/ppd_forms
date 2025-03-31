88331f6cb8f9857e1a0ec162d85336f687cddea8
sidelkin <sidelkin@yandex.ru>
2025-03-31 13:52:04 +0400

    🐛 fix(db): handle non-layer words in well test layer mapper

--- app/infrastructure/db/mappers/well_test_layer.py
+++ app/infrastructure/db/mappers/well_test_layer.py
@@ -7,6 +7,7 @@ class WellTestLayerMapper(BaseMapper):
     layer_pattern: re.Pattern = re.compile(
         r"[\w+\-()]+\s+(?P<layer>[\w+\-()`]+)"
     )
+    not_layers_actually = ("Восточного", "Рудинского")
 
     def replace_word(
         self, word: WordOrder, max_order: int
@@ -14,6 +15,8 @@ class WellTestLayerMapper(BaseMapper):
         new_value = word[self.WORD]
         if match := self.layer_pattern.match(new_value):
             new_value = match.group("layer")
+            if new_value in self.not_layers_actually:
+                new_value = ""
         else:
             new_value = ""
         return (new_value, word[self.ORDER]), max(max_order, word[self.ORDER])
